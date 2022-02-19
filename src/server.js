fec4162fa2c161133001f6060722226895356eb3
Rafael CNC <rafael.cechinato@pucpr.edu.br>
2022-02-19 18:29:40 -0300

    Change port

--- src/server.js
+++ src/server.js
@@ -9,4 +9,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(9000, () => console.log("Server started"))
+app.listen(process.env.PORT || 5000, () => console.log("Server started"))
